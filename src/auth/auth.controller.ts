e14369f5fabb5c014e99e37b16323cf95dba2da9
SegunCodes <shegstix64@gmail.com>
2024-05-20 20:41:46 +0100

    auth controller and guards

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Request, Post, UseGuards, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @Post('register')
+  async register(@Body() createUserDto: { email: string; password: string }) {
+    return this.authService.register(createUserDto);
+  }
+}
